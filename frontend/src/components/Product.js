232e71158c4c3cb4ba5a2617889488a0e5939813
iamajeeth <rajeethkumarind01@gmail.com>
2021-08-29 11:17:42 +0530

    Rating component init

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
@@ -11,12 +12,14 @@ const Product = ({product}) => {
             <Card.Body>
             <a href={`/Product/${product._id}`}>
                 <Card.Title as='div'>
-                
                     <strong>{product.name}</strong>
                 </Card.Title>
             </a>
-            <Card.Text as='div'><div className='my-3'>{product.rating} from {product.numReviews} reviews</div></Card.Text>
-            <Card.Text as='h3'>INR {product.price}</Card.Text>
+            <Card.Text as='div'>
+                    <Rating value={product.rating} 
+                     text={`${product.numReviews} reviews`} />
+                </Card.Text>
+            <Card.Text as='h3'>₹ {product.price}</Card.Text>
              </Card.Body>
         </Card>
     )
