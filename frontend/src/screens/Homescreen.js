bafcdf62511d6aa631d11e38f30c5b741838b3b6
iamajeeth <rajeethkumarind01@gmail.com>
2021-08-30 10:59:37 +0530

    acess data from backend in react

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+
 const Homescreen = () => {
+  const [products, setProducts] = useState([])
+
+  const useEffect =
+    (() => {
+      const fetchProducts = async () => {
+        const { data } = await axios.get('/api/products')
+        setProducts(data)
+      }
+
+      fetchProducts()
+    },
+    [])
+
   return (
     <>
       <h1>Latest products</h1>
