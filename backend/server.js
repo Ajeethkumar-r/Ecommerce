2a62e4e9ed6ff91b69437d2778090435ed03b901
iamajeeth <rajeethkumarind01@gmail.com>
2021-08-31 20:30:12 +0530

    set env for node and port

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 const app = express()
 
+dotenv.config()
+
 app.get('/', (req, res) => {
-  res.send('API is running in port 5000')
+  res.send('API is running in port 5000...')
 })
 
 app.get('/api/products', (req, res) => {
@@ -15,4 +18,11 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('API running in port 5000...'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `sever running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
+)
