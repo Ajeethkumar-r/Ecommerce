c5fd6a3f719df68ae40b85642519473dde4c85bc
iamajeeth <rajeethkumarind01@gmail.com>
2021-09-01 16:21:06 +0530

    type as string in -p users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,20 +4,20 @@ const users = [
   {
     name: 'Admin user',
     email: 'admin@example.com',
-    password: bcrypt.hashSync(123456, 10), // to make our password secure we need to hash it using 'hashSync' function along with the bcrypt package
+    password: bcrypt.hashSync('123456', 10), // to make our password secure we need to hash it using 'hashSync' function along with the bcrypt package
     isAdmin: true, //to verify the admin user
   },
   {
     //demo user
     name: 'ajeeth',
     email: 'ajeeth@example.com',
-    password: bcrypt.hashSync(123456, 10),
+    password: bcrypt.hashSync('123456', 10),
   },
   {
     //demo user
     name: 'ranjith',
     email: 'ranjith@example.com',
-    password: bcrypt.hashSync(123456, 10),
+    password: bcrypt.hashSync('123456', 10),
   },
 ]
 
