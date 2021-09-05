5a4f0afce6b9eaca4db6781e7543dab82cdde889
iamajeeth <rajeethkumarind01@gmail.com>
2021-09-05 20:52:39 +0530

    action for cart reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,20 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+
 const reducer = combineReducers({
   productList: productListReducer, // Get our 'productListReducer' for 'products' we have and set it to gobal state container using 'combineReducer()'
   productDetails: productDetailsReducer, // Get our 'productDetailsReducer' for 'single product' we have and set it to gobal state container using 'combineReducer()'
+  cart: cartReducer,
 })
 
-const intialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems')) // we parse the localStorage JSON file to use here
+  : []
+const intialState = {
+  cart: { cartIems: cartItemsFromStorage }, // all the products are available as intialState bcoz  :> while we adding the product to the cart it is done by using the products we already have through 'productList and productDetails'
+}
 
 const middleware = [thunk]
 
