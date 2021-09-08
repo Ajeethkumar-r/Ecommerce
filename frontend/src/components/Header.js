8ab65fa2e3871ae8d4aa96f5c4fea596de0a173e
iamajeeth <rajeethkumarind01@gmail.com>
2021-09-08 14:15:51 +0530

    delete items in cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,34 @@
 import React from 'react'
-import {LinkContainer} from 'react-router-bootstrap'
-import {Container,Navbar,Nav} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 const Header = () => {
-    return (
-        <header>
-            
-            <Navbar bg="dark"  variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <LinkContainer to='/'>
-                <Navbar.Brand >Gshop</Navbar.Brand>
-                </LinkContainer>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navi">
-                        
-                    <LinkContainer to='/Cart'>
-                        <Nav.Link >Cart</Nav.Link>
-                    </LinkContainer>
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Gshop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle arial-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='navi'>
+              <LinkContainer to='/Cart'>
+                <Nav.Link>
+                  <span className='fas fa-shopping-cart'></span> Cart
+                </Nav.Link>
+              </LinkContainer>
 
-                    <LinkContainer to='/login'>
-                        <Nav.Link href="/Login">Sign-in</Nav.Link>
-                    </LinkContainer>
-                    </Nav> 
-                </Navbar.Collapse>
-            </Container>   
-            </Navbar>
-        
-        
-        </header>
-    )
+              <LinkContainer to='/login'>
+                <Nav.Link href='/Login'>
+                  <span className='fas fa-user'></span> Sign-in
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
