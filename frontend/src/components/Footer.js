ec4382615f2e38a9965e0e990f10f3cb002a7290
iamajeeth <rajeethkumarind01@gmail.com>
2021-09-12 14:49:48 +0530

    paymentscreen columns

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Container,Row,Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className='text-center py-3'>Copyright &copy; Myshop
-                    </Col>
-                </Row>
-            </Container>
-            
-        </footer>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; Gshop</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
