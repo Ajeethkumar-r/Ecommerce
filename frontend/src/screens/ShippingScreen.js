b0a0af761ba7c95df5b333c7cf068f67f3f470e5
iamajeeth <rajeethkumarind01@gmail.com>
2021-09-12 10:05:12 +0530

    shipping action and constant

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -10,17 +10,74 @@ const ShippingScreen = ({ history }) => {
   const [placeState, setPlaceState] = useState('')
   const [city, setCity] = useState('')
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('hello')
+  }
+
   return (
     <FormContainer>
-      <Form.Group controlId='name'>
-        <Form.Label>Name</Form.Label>
-        <Form.Control
-          type='name'
-          placeholder='Enter Name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></Form.Control>
-      </Form.Group>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='address'>
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter address'
+            value={address}
+            required
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='postalcode'>
+          <Form.Label>Postal Code</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter postal code'
+            value={postalcode}
+            required
+            onChange={(e) => setPostalCode(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='city'>
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter city'
+            value={city}
+            required
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='placestate'>
+          <Form.Label>State</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter state'
+            value={placeState}
+            required
+            onChange={(e) => setPlaceState(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='country'>
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter country'
+            value={country}
+            required
+            onChange={(e) => setCountry(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='primary'>
+          Cotinue
+        </Button>
+      </Form>
     </FormContainer>
   )
 }
