f38321d1af1881b5ea1a10ce6023dcdaf5120498
iamajeeth <rajeethkumarind01@gmail.com>
2021-09-27 15:10:30 +0530

    searchbox refresh

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,36 +1,35 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
 const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState('')
 
-    const [keyword,setKeyword] = useState('')
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-        if (keyword) {
-            history.push(`/search/${keyword}`)
-        } else {
-            history.push('/')
-        }
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (keyword) {
+      history.push(`/search/${keyword}`)
+      setKeyword('')
+    } else {
+      history.push('/')
     }
+  }
 
-    return (
-        <Form onSubmit={submitHandler} className='d-flex flex-nowrap '>
-            <Form.Control
-                type='q'
-                name='search'
-                onChange={(e) => setKeyword(e.target.value)}
-                placeholder='search...'
-            className='mr-2-sm ml-5-sm  p-1 '>
-            </Form.Control>
-            <span className='p-1'></span>
-           
-            <Button type='submit' variant='outline-success' className='p-1' >
-                Search
-            </Button>
-          
-       </Form>
-    )
+  return (
+    <Form onSubmit={submitHandler} className='d-flex flex-nowrap '>
+      <Form.Control
+        type='q'
+        name='search'
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder='search...'
+        className='mr-2-sm ml-5-sm  p-1 '
+      ></Form.Control>
+      <span className='p-1'></span>
+
+      <Button type='submit' variant='outline-success' className='p-1'>
+        Search
+      </Button>
+    </Form>
+  )
 }
 
 export default SearchBox
